ec93aadbd8647d344cc56ec624bb1a3781f99956
Abydzar <ukrabka@gmail.com>
2026-04-20 09:44:10 +0700

    Refactor soal1.c for sensor data recovery
    Added comments and improved the logic for handling sensor data recovery.

--- soal1.c
+++ soal1.c
@@ -1,5 +1,46 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 2
+ *   Hari dan Tanggal    : Senin, 20 April 2026
+ *   Nama (NIM)          : Abydzar Dzakyan Akbar
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Program yang dapat mengisi data sensor yang kosong sesuai dengan kondisi yang diberikan
+ *                          lalu melakukan perkalian untuk nilai-nilai yang telah dibenarkan
+ * 
+ */
 
-int main(void) {
+
+ #include <stdio.h>
+ #include <string.h>
+
+
+ int main() {
+
+    int arr_size;
+    //printf("");
+    scanf("%d ", arr_size);
+    int arrdat[arr_size];
+    int *parrdat[arr_size];
+    int i;
+
+    for (i = 0; i<arr_size; i++){
+        scanf("%d", &arrdat[i]);
+        parrdat[i] = &arrdat[i];
+    }
+
+    if (arrdat[i] == -1){
+        if (arrdat[i-1] != -1 && arrdat[i+1] != -1){
+            int ada = (arrdat[i-1] + arrdat[i+1])/2;
+            parrdat[i] = &ada;
+        } else if (arrdat[i-1] != -1 && arrdat[i+1]==-1){
+            parrdat[i] = &arrdat[i-1];
+        } else if (arrdat[i+1 != -1 && arrdat[i-1]==-1]){
+            parrdat[i] = &arrdat[i+1];
+        } else if (arrdat[i-1] == -1 && arrdat[i+1] == -1){
+            parrdat[i] = 0;
+        }
+
+    }
+    printf("RECOVERED %d", arrdat);
     return 0;
+ 
 }
